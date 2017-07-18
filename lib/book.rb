c3bf45cf04ef58ea2f4dd80c4fd0c26dee14199e
Joe Cha <josephmcha@gmail.com>
2017-07-18 18:15:47 -0400

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book
 
+def initialize(first)
+  @title = first
 
+end
+
+def title
+  @title
+end
+
+def author= (authorname)
+  @author = authorname
+end
+
+def author
+  @author
+end
+def page_count= (num)
+  @page_count = num
+end
+def page_count
+  @page_count
+end
+
+def genre= (theme)
+  @genre = theme
+
+end
+
+def genre
+@genre
+end
+
+def turn_page
+puts "Flipping the page...wow, you read fast!"
+ end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,55 @@
 # Make your shoe class here!
+
+class Shoe
+
+  def initialize(first)
+    @brand = first
+
+  end
+
+def brand
+  @brand
+end
+
+def color= (it)
+  @color = it
+
+end
+
+def color
+  @color
+end
+
+
+def size= (num)
+  @size = num
+end
+
+def size
+  @size
+end
+
+def material= (mat)
+  @material = mat
+end
+
+def material
+  @material
+end
+
+def condition= (cond)
+  @condition = cond
+end
+
+def condition
+  @condition
+end
+
+def cobble
+  puts "Your shoe is as good as new!"
+  itself.condition = "new"
+end
+
+
+
+end
